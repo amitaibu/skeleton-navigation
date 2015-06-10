1b76ca3622e3a638955cfc96c8bc7b6ecc8e3ef8
Amitai Burstein <amitai@gizra.com>
2015-06-10 23:59:49 +0300

    Add cache

--- src/services/builds-api.js
+++ src/services/builds-api.js
@@ -2,10 +2,21 @@ import {WebAPI} from './backend-http';
 
 export class BuildsApi extends WebAPI {
 
+  cache = null;
+
   get() {
     this.isRequesting = true;
+
+    if (this.cache) {
+      return Promise.resolve(this.cache);
+    }
+
     return this
       .http
-      .get('api/github_repos');
+      .get('api/github_repos')
+      .then(response => {
+        this.cache = response;
+        return response;
+      })
   }
 }
