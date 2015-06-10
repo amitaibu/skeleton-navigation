6468070b9ec8f9258d3024a6a641317fe25043a2
Amitai Burstein <amitai@gizra.com>
2015-06-10 23:46:52 +0300

    Add config

--- src/services/backend-http.js
+++ src/services/backend-http.js
@@ -1,14 +1,16 @@
 import {inject} from 'aurelia-framework';
 import {HttpClient} from 'aurelia-http-client';
+import {Config} from '../config/config';
 
+@inject(Config)
 export class WebAPI {
 
-  constructor(http){
+  constructor(config){
     this.http = new HttpClient();
 
     this.http
       .configure(x => {
-        x.withBaseUrl('http://localhost/shoov/www');
+        x.withBaseUrl(config.backendUrl);
         x.withHeader('access-token', localStorage.getItem('access_token'));
     });
   }
